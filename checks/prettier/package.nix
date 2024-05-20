842c3f225677aa55e44b94342f19c8f3e6f2be06
Isabel <isabel@isabelroses.com>
2024-05-20 09:24:10 +0100

    feat: add prettier check (#1)
    * feat: prettier check
    
    * chore: use prettier to format ci file

---
+++ checks/prettier/package.nix
@@ -0,0 +1,11 @@
+{
+  lib,
+  runCommand,
+  root,
+  nodePackages,
+}:
+runCommand "check-prettier" {} ''
+  cd ${root}
+  ${lib.getExe nodePackages.prettier} --ignore-path '.git' --check .
+  touch $out
+''
